f671ec38377b20ffd194886618c9ddfb30e35ba1
onetrckchris <onetrckchris@gmail.com>
2019-07-23 12:12:51 -0700

    created react app and removed any unwanted files. Also created basic reducer

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: Date.now
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
